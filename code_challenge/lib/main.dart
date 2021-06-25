8720f71888296c3584f4f24357659d34a26172e9
Vladislav Taraikovich <54981764+soulsodev@users.noreply.github.com>
2021-06-25 23:56:35 +0300

    fix: spltitting

--- code_challenge/lib/main.dart
+++ code_challenge/lib/main.dart
@@ -3,19 +3,25 @@ import 'dart:io';
 import 'pizza_bot.dart';
 
 void main(List<String> arguments) {
-  // Input: 5x5 (0,0) (1,3) (4,4) (4,2) (4,2) (0,1) (3,2) (2,3) (4,1)
+  // Input: 5x5 (1, 3) (4, 4)
+  // Output: ENNNDEEEND
+  // Input: 5x5 (0, 0) (1, 3) (4, 4) (4, 2) (4, 2) (0, 1) (3, 2) (2, 3) (4, 1)
   // Output: DENNNDEEENDSSDDWWWWSDEEENDWNDEESSD
-  // 5x5 (0,0) (1,1)
+
   print('Enter input string:');
   var readLine = stdin.readLineSync();
-  var line = readLine!.replaceAll('(', '').replaceAll(')', '').split(' ');
+  var line = readLine!.replaceAll(')', '').split(' (');
   var size = line[0].split('x').map(int.parse).toList();
 
   var coordinates = <List<int>>[];
   var points = line.sublist(1);
   for (var i = 0; i < points.length; i++) {
-    var point = points[i].split(',').map(int.parse).toList();
-    if (point[0] >= size[0] || point[0] < 0 || point[1] >= size[1] || point[1] < 0) {
+    var point =
+        points[i].split(',').map((e) => e.trim()).map(int.parse).toList();
+    if (point[0] >= size[0] ||
+        point[0] < 0 ||
+        point[1] >= size[1] ||
+        point[1] < 0) {
       throw Exception('Wrong coordinates.');
     }
     coordinates.add(point);
